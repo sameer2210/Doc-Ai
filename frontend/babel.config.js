75dfa8a581c5729836589aa22074a01e87caeeaf
SAM <sameerkhan27560@gmail.com>
2026-05-31 00:55:48 +0530

    apk build fail

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,8 +1,17 @@
+// module.exports = function (api) {
+//   api.cache(true);
+
+//   return {
+//     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+//     plugins: ['react-native-reanimated/plugin'],
+//   };
+// };
+
 module.exports = function (api) {
   api.cache(true);
 
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }]],
     plugins: ['react-native-reanimated/plugin'],
   };
 };
